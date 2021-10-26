c0be0c139a73a11beb1f3241211f59444c0df5ef
Nandan Bhat <48284547+mynamedoesntfi@users.noreply.github.com>
2021-10-26 09:27:23 +0530

    placeholder for jpa config in application.properties

--- src/main/java/com/example/Birthday_JobAnniversary_WisherBackend/BirthdayJobAnniversaryWisherBackendApplication.java
+++ src/main/java/com/example/Birthday_JobAnniversary_WisherBackend/BirthdayJobAnniversaryWisherBackendApplication.java
@@ -1,10 +1,13 @@
 package com.example.Birthday_JobAnniversary_WisherBackend;
 
+import com.example.Birthday_JobAnniversary_WisherBackend.Repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class BirthdayJobAnniversaryWisherBackendApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/Birthday_JobAnniversary_WisherBackend/Repositories/UserRepository.java
@@ -0,0 +1,4 @@
+package com.example.Birthday_JobAnniversary_WisherBackend.Repositories;
+
+public class UserRepository {
+}
