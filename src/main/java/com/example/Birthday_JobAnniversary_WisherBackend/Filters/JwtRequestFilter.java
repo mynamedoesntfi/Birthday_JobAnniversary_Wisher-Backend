c40b28eecbdf92c4cad05e96d12e9a75714efd52
Saba-d-coder <saba100.ksk@gmail.com>
2021-10-26 19:46:23 +0530

    updates done

--- src/main/java/com/example/Birthday_JobAnniversary_WisherBackend/Filters/JwtRequestFilter.java
+++ src/main/java/com/example/Birthday_JobAnniversary_WisherBackend/Filters/JwtRequestFilter.java
@@ -28,36 +28,6 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//        final String authorizationHeader = request.getHeader("Authorization");
-//
-//        String username = null;
-//        String jwt = null;
-//
-//        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-//            jwt = authorizationHeader.substring(7);
-//            username = jwtUtilService.extractUsername(jwt);
-//        }
-//
-//        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-//            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-//
-//            if(jwtUtilService.validateToken(jwt, userDetails)) {
-//
-//                /**
-//                 * Following is stuff spring would do by default, but have to do it manually since we are
-//                 * validating the jwt (validating jwt is not part of default behaviour)
-//                 */
-//                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-//                        new UsernamePasswordAuthenticationToken(username,userDetails.getPassword(), userDetails.getAuthorities());
-//
-//                usernamePasswordAuthenticationToken
-//                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-//                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-//            }
-//        }
-//
-//        /** To continue remaining filter operations on the request */
-//        filterChain.doFilter(request, response);
         final String requestTokenHeader = request.getHeader("Authorization");
 
         String username = null;
@@ -83,7 +53,10 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
             if (jwtUtilService.validateToken(jwtToken, userDetails)) {
 
-//              store the authToken in context
+                /**
+                 * Following is stuff spring would do by default, but have to do it manually since we are
+                 * validating the jwt (validating jwt is not part of default behaviour)
+                 */
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
                         username, userDetails.getPassword(), userDetails.getAuthorities());
 
@@ -97,6 +70,8 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
+
+        /** To continue remaining filter operations on the request */
         filterChain.doFilter(request, response);
     }
 }
